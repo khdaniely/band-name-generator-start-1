82a10edc0f2c4567241cc19f70e8a77352c25871
Kenneth Daniely <>
2021-05-31 22:17:10 +0000

    nice job and no cheating! Good for you!

--- main.py
+++ main.py
@@ -8,3 +8,20 @@
 
 #5. Make sure the input cursor shows on a new line, see the example at:
 #   https://band-name-generator-end.appbrewery.repl.run/
+
+# # Greeting line
+print("Welcome to the band generator! ")
+# # User will enter the city of their choice
+city_name = input("What city are you from? ")
+# # user will enter the city of their choice
+pet_name = input("What is the name of your pet? ")
+# Band generator results
+# bgr = city_name + pet_name
+print("Your band name could be", city_name, pet_name)
+# # Prints the results for the band generator
+# print("Your band name could be " ) + (city_name + pet_name))
+
+
+# f_name = input("What is your name? ")
+# l_name = input("What is your last name? ")
+# print(f_name, l_name)
